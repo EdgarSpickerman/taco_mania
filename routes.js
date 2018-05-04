5fdfb29bd824ecf6dcdcd1e8ebc95bdb0aedf380
Edgar Spickerman <spickerman.edgar@gmail.com>
2018-05-03 22:36:36 -0700

    added index view unstyled

--- routes.js
+++ routes.js
@@ -1,12 +1,16 @@
 const router = require("express").Router();
 const tacoController = require("./controllers/taco_controller");
 
+const showView = (data, res, template) => {
+  const dataObject = {}
+  dataObject.results = data
+  res.render(template, dataObject);
+}
+
 router
   .get("/", (req, res, next) => {
     tacoController.getAll()
-      .then(queryResults => {
-        res.render("index.handlebars", {results: queryResults})
-      })
+      .then(data => showView(data, res, "index.handlebars"))
       .catch(err => console.log(err))
   })
 
